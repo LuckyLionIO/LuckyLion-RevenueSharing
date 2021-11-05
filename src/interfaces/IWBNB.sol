4fa2c07f114aaadc57ddd6d703fb400d0e20dd0f
pattharathon srithundorn <lemon.pattharathon@gmail.com>
2021-11-05 15:51:27 +0700

    Remove unused file

--- src/interfaces/IWBNB.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity >=0.5.0;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IWBNB is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint256) external;
-}
